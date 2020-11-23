7176e25828324e925c4546a37552fd5f85db704e
thangnv7975 <thangnv7975.vnist@gmail.com>
2020-11-23 15:22:58 +0700

    gitignore

--- connectDatabase.js
+++ connectDatabase.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const db = `mongodb://localhost:27017/monitoredObject`;
+const db =`mongodb+srv://thangnguyen:thangnguyen26@cluster0.sxzso.mongodb.net/monitoredObject?retryWrites=true&w=majority`
 const optionConnectDB = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -8,6 +8,6 @@ const optionConnectDB = {
 };
 
 mongoose // Connect to MongoDB
-    .connect(process.env.DATABASE || db, optionConnectDB)
+    .connect(db, optionConnectDB)
     .then(() => console.log("Kết nối dữ liệu thành công"))
     .catch((err) => console.log(err));
