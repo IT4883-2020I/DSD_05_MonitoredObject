3eef08e4f2624ca3765b128d1b3d6b877207a0fc
thangnv7975 <thangnv7975.vnist@gmail.com>
2020-11-23 16:51:16 +0700

    gitignore

--- modules/controllers/monitoredObject.controller.js
+++ modules/controllers/monitoredObject.controller.js
@@ -39,7 +39,7 @@ exports.getDetailMonitoredObject = async (req, res) => {
 
 exports.createMonitoredObject = async (req, res) => {
     try {
-        const monitoredObject = MonitoredObjectService.createMonitoredObject(req.body);
+        const monitoredObject = await MonitoredObjectService.createMonitoredObject(req.body);
 
         res.status(200).json({
             success: true,
